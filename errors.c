87047e720f5a2f29fe344a5575c1853a1b4da6b5
Dennis Chiira <programmingchiira@gmail.com>
2023-05-18 11:37:21 -0700

    We Had To Use Dark Magic To Make This Wor

---
+++ errors.c
@@ -0,0 +1,83 @@
+#include "shell.h"
+
+/**
+ *_eputs - Prints a string
+ * @str: String to printed
+ * Return: Nothing
+ */
+
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - Writes c to stderr
+ * @c: Character to print
+ * Return: 1 on success, -1 on error with appropriate errno.
+ */
+
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - Writes 'c' to the specified (fd)
+ * @c: Character to print
+ * @fd: Filedescriptor to write to
+ * Return: 1 on success, -1 on error with appropriate errno.
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - Prints string to the specified (fd)
+ * @str: String to printed
+ * @fd: fd to write to
+ * Return: Number of characters written
+ */
+
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
---
+++ getenv.c
@@ -0,0 +1,92 @@
+#include "shell.h"
+
+/**
+ * get_environ - A copy of the string array representing environ
+ * @info: Has arguments, maintaining constant function prototype
+ * Return: Always 0
+ */
+
+char **get_environ(info_t *info)
+{
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
+
+	return (info->environ);
+}
+
+/**
+ * _unsetenv - Deletes environ variable
+ * @info: Has arguments, maintaining function prototype
+ * Return: 1 if deleted, 0 otherwise
+ * @var: Variable to remove
+ */
+
+int _unsetenv(info_t *info, char *var)
+{
+	list_t *node = info->env;
+	size_t i = 0;
+	char *p;
+
+	if (!node || !var)
+		return (0);
+
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			info->env_changed = delete_node_at_index(&(info->env), i);
+			i = 0;
+			node = info->env;
+			continue;
+		}
+		node = node->next;
+		i++;
+	}
+	return (info->env_changed);
+}
+
+/**
+ * _setenv - Initialize or update an environ var
+ * @info: Has arguments, ensuring consistent function prototype
+ * @var: Variable name
+ * @value: Variable value
+ *  Return: Always 0
+ */
+
+int _setenv(info_t *info, char *var, char *value)
+{
+	char *buf = NULL;
+	list_t *node;
+	char *p;
+
+	if (!var || !value)
+		return (0);
+
+	buf = malloc(_strlen(var) + _strlen(value) + 2);
+	if (!buf)
+		return (1);
+	_strcpy(buf, var);
+	_strcat(buf, "=");
+	_strcat(buf, value);
+	node = info->env;
+	while (node)
+	{
+		p = starts_with(node->str, var);
+		if (p && *p == '=')
+		{
+			free(node->str);
+			node->str = buf;
+			info->env_changed = 1;
+			return (0);
+		}
+		node = node->next;
+	}
+	add_node_end(&(info->env), buf, 0);
+	free(buf);
+	info->env_changed = 1;
+	return (0);
+}
